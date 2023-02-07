4c82d1370211fcdceac60f2780dc02635197398b
CarlosDahmen <carlos.dahmen@gmail.com>
2023-02-07 12:29:23 -0500

    Rearranged keypad layout

---
+++ src/components/Keypad.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import { keyboardLetters } from "../utilities/letters";
+
+const Keypad = ({usedKeys}) => {
+  const [letters, setLetters] = useState(null)
+
+  useEffect(() => {
+    setLetters(keyboardLetters)
+  }, [])
+
+  return (
+    <div className="keypad">
+      {letters && letters.map((letter) => {
+        const color = usedKeys[letter.key.toUpperCase()]
+        return (
+          <div key={letter.key} className={color}>{letter.key}</div>
+          )
+        })}
+    </div>)
+}
+
+export default Keypad
