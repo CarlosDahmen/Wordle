f2499ea7d5dbf7ce20b2d4cf6fa1dbee1fb4030c
CarlosDahmen <carlos.dahmen@gmail.com>
2023-02-08 11:20:44 -0500

    Remove Header Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import "./styles/App.scss";
 import { hot } from "react-hot-loader";
-import Header from "./components/Header";
 import Game from "./components/Game";
 import { GameDetailsProvider } from "./context/GameDetails";
 
 const App = () => {
   return (
     <GameDetailsProvider>
-      <Header />
       <Game />
     </GameDetailsProvider>
   );
--- src/components/Header.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Header = () => <header className="App-header">Wordle</header>;
-
-export default Header;
