3820343436a31570e499243bb68fa0ec6286d6ac
CarlosDahmen <carlos.dahmen@gmail.com>
2023-02-15 11:48:22 -0500

    Update userEvent version

--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,64 @@
-const path = require('path');
+const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const webpack = require("webpack");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
     app: [
-      path.join(__dirname, './src/index.tsx'),
-      'webpack-hot-middleware/client?path=/__webpack_hmr&reload=true',
+      path.join(__dirname, "./src/index.jsx"),
+      "webpack-hot-middleware/client?path=/__webpack_hmr&reload=true",
     ],
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    hotUpdateChunkFilename: '.hot/hot-update.js',
-    hotUpdateMainFilename: '.hot/hot-update.json',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    hotUpdateChunkFilename: ".hot/hot-update.js",
+    hotUpdateMainFilename: ".hot/hot-update.json",
   },
   watchOptions: {
-    ignored: '/node_modules/',
+    ignored: "/node_modules/",
   },
   resolve: {
     alias: {
-      'react-dom': '@hot-loader/react-dom',
+      "react-dom": "@hot-loader/react-dom",
     },
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
   module: {
     rules: [
-        {
-            test: /\.(js|mjs|jsx|ts|tsx)$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                  presets: ['@babel/preset-env', '@babel/preset-react'],
-                  plugins: ['react-hot-loader/babel'],
-                }
-            }
+      {
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: ["react-hot-loader/babel"],
+          },
         },
-        {
-          test: /\.tsx?$/,
-          use: 'ts-loader',
-          exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.svg/,
+        use: {
+          loader: "svg-url-loader",
+          options: {
+            // make all svg images to work in IE
+            iesafe: true,
+          },
         },
-        {
-            test: /\.scss$/i,
-            use: ["style-loader", "css-loader", "sass-loader"],
-        },
-        {
-            test: /\.svg/,
-            use: {
-                loader: "svg-url-loader",
-                options: {
-                    // make all svg images to work in IE
-                    iesafe: true,
-                },
-            },
-        },
-    ]
+      },
+    ],
   },
   plugins: [
     new CopyPlugin({
-        patterns: [
-          { from: "public", to: "public"},
-          { from: "src/index.html"}
-        ],
-      }),
+      patterns: [{ from: "public", to: "public" }, { from: "src/index.html" }],
+    }),
     new CleanWebpackPlugin(),
     new webpack.HotModuleReplacementPlugin(),
   ],
